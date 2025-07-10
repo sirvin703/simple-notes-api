4301b7e49154590d1fd1cd3dd780e2697c9ccabc
Shaun Irvin <sirvin@uchicago.edu>
2025-07-09 23:33:16 -0500

    more test app changes

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
 import sys
 import os
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from app import app
+
+base_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, base_dir)
 
 
 # Test 1
